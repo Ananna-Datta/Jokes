a2fa20b1e813b87e475f09211a55c1d9832ac0bb
Ananna-Datta <anannadatta45@gmail.com>
2024-04-21 20:43:58 +0600

    cors add

--- JokesApi/settings.py
+++ JokesApi/settings.py
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = ['https://jokes-tgdo.onrender.com','https://*.127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
+CORS_ALLOWED_ORIGINS=True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
